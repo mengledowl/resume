979667741a51f83d9a09670807fb7a378ca29ffb
Matt Engledowl <mengledowl@gmail.com>
2016-11-19 00:02:15 -0600

    Add public resume w/ public-safe contact info

--- lib/generate_html.rb
+++ lib/generate_html.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/env ruby
 
-template_file = File.open('lib/resume-template.html', 'rb')
-template = template_file.read
-template_file.close
+def generate(footer, destination)
+  template_file = File.open('lib/resume-template.html', 'rb')
+  template = template_file.read
+  template_file.close
 
-footer_file = File.open('lib/_footer.html', 'rb')
-footer = footer_file.read
-footer_file.close
+  footer_file = File.open(footer, 'rb')
+  footer = footer_file.read
+  footer_file.close
 
-template.gsub!('{{footer}}', footer)
+  template.gsub!('{{footer}}', footer)
 
-Dir.mkdir('tmp') unless File.exists?('tmp')
-File.open('tmp/resume.html', 'w') { |file| file.write(template) }
+  Dir.mkdir('tmp') unless File.exists?('tmp')
+  File.open(destination, 'w') { |file| file.write(template) }
+end
+
+generate('lib/_footer.html', 'tmp/resume.html')
+generate('lib/_public_footer.html', 'tmp/public-resume.html')
+
+
--- resume_to_pdf.sh
+++ resume_to_pdf.sh
@@ -3,3 +3,4 @@
 ruby lib/generate_html.rb
 mkdir -p bin
 wkhtmltopdf --margin-bottom 0mm --margin-left 0mm --margin-right 0mm --margin-top 0mm tmp/resume.html bin/resume.pdf
+wkhtmltopdf --margin-bottom 0mm --margin-left 0mm --margin-right 0mm --margin-top 0mm tmp/public-resume.html bin/public-resume.pdf
